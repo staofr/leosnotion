5b4a63280a637d826227d650a5655c707f33e78b
staofr <101335940+staofr@users.noreply.github.com>
2024-01-31 18:17:20 +0100

    2th update

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'm@leosun.fr',
   link: 'https://blog.leosun.fr',
   description: 'ça va être un super blog.',
-  lang: 'fr-FR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Europe/Paris', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
